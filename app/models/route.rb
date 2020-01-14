e72853722da2a2451af601e9566d099e579e85ae
Adrien Di Pasquale <adrien@dipasquale.fr>
2020-01-14 21:45:57 +0100

    fix route association to ships

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,7 +1,8 @@
 class Route < ApplicationRecord
   belongs_to :city_a, class_name: 'City'
   belongs_to :city_b, class_name: 'City'
-  has_and_belongs_to_many :ships
+  has_many :ship_routes
+  has_many :ships, through: :ship_routes
 
   def to_s
     "#{city_a} - #{city_b}"
