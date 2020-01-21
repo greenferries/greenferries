8ad7340255ff7b45e5cd63c301588333b61733a1
Adrien Di Pasquale <adrien@dipasquale.fr>
2020-01-21 17:20:50 +0100

    added uniqueness checks for routes and cities

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,8 @@
 class City < ApplicationRecord
   acts_as_mappable lat_column_name: :latitude, lng_column_name: :longitude
 
+  validates_uniqueness_of :geonames_id
+
   def to_s
     "[#{country}] #{name}"
   end
--- app/models/route.rb
+++ app/models/route.rb
@@ -4,6 +4,8 @@ class Route < ApplicationRecord
   has_many :ship_routes
   has_many :ships, through: :ship_routes
 
+  validate :unique?
+
   def self.human_ordered
     joins('LEFT JOIN cities AS cities_a ON cities_a.id = city_a_id').
       joins('LEFT JOIN cities AS cities_b ON cities_b.id = city_b_id').
@@ -12,5 +14,14 @@ class Route < ApplicationRecord
 
   def to_s
     "#{city_a} - #{city_b}"
+  end
+
+  def unique?
+    if Route.where(city_a_id: city_a_id, city_b_id: city_b_id).
+        or(Route.where(city_a_id: city_b_id, city_b_id: city_a)).
+        where.not(id: id).
+        any?
+      errors.add(:city_a, 'Route already exists for these cities')
+    end
   end
 end
