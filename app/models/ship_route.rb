e80a26f9ac0d480008fda91832f3ab2e5b936eae
Adrien Di Pasquale <adrien@dipasquale.fr>
2020-01-26 16:10:11 +0100

    added uniqueness check on ship routes

--- app/models/ship_route.rb
+++ app/models/ship_route.rb
@@ -1,4 +1,6 @@
 class ShipRoute < ApplicationRecord
   belongs_to :ship, counter_cache: :routes_count
   belongs_to :route, counter_cache: :ships_count
+
+  validates :ship, uniqueness: { scope: :route }
 end
