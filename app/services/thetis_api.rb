d292e487588c9481b31a8331bd5c8a368c26ac2a
Adrien Di Pasquale <adrien@dipasquale.fr>
2020-02-13 20:02:21 +0100

    fixing thetis url

--- app/services/thetis_api.rb
+++ app/services/thetis_api.rb
@@ -1,5 +1,5 @@
 class ThetisApi
-  BASE_URL = 'http://data.greenferries.org/thetis/ships.json'
+  BASE_URL = 'https://data.greenferries.org/thetis/ships.json'
   # BASE_URL = 'http://127.0.0.1:8001/thetis/ships.json'
   def self.get_ship_by_imo(imo)
     url = BASE_URL + "?imo__exact=#{imo}&_shape=objects"
