90993baa867ae1f706a92386ca3c8267d708a83f
Adrien Di Pasquale <adrien@dipasquale.fr>
2020-12-13 11:26:57 +0100

    show EIV and EEDI in ship pages

--- data/scripts/www.py
+++ data/scripts/www.py
@@ -37,15 +37,11 @@ def monitoring_methods(row):
             monitoring_methods.append(letter)
     return monitoring_methods
 
-def get_thetis_df():
-    df_thetis = pd.read_csv(THETIS_CSV_PATH, dtype={"imo": str}).replace({np.nan: None})
+def write_thetis_jsons():
+    df_thetis = pd.read_csv(THETIS_CSV_PATH).replace({np.nan: None})
     df_thetis["monitoring_methods"] = df_thetis.apply(lambda row: monitoring_methods(row), axis=1)
     for letter in ["a", "b", "c", "d"]:
         del df_thetis[f"monitoring_method_{letter}"]
-    return df_thetis
-
-def write_thetis_jsons():
-    df_thetis = pd.read_csv(THETIS_CSV_PATH).replace({np.nan: None})
     df_thetis.columns = df_thetis.columns.map(lambda x: camelCase(x))
     filenames = os.listdir(WWW_SHIPS_PATH)
     matching_filenames = [fn for fn in filenames if re.match(r".*-(\d+)\.md", fn)]
@@ -61,7 +57,10 @@ def write_thetis_jsons():
             json.dump(data, f, indent=2)
 
 def export_monitoring_methods_graph():
-    df_thetis = get_thetis_df()
+    df_thetis = pd.read_csv(THETIS_CSV_PATH, dtype={"imo": str}).replace({np.nan: None})
+    df_thetis["monitoring_methods"] = df_thetis.apply(lambda row: monitoring_methods(row), axis=1)
+    for letter in ["a", "b", "c", "d"]:
+        del df_thetis[f"monitoring_method_{letter}"]
     md_imos = get_frontmatter_df(exclude_out_of_scope=True).imo.apply(lambda x: str(x))
     df_ferries = df_thetis[df_thetis.imo.isin(md_imos)]
     df_ferries_2019 = df_ferries[df_ferries.reporting_period == 2019]
